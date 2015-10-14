28c95797d37dc69468b028ad88d15e7f104294d3
rochness <rochelle.y.lee@gmail.com>
2015-10-14 15:39:07 -0700

    BlinkyDancer and Dancer successfully refactored in pseudoclassical form

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -6,7 +6,7 @@ describe("blinkyDancer", function() {
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+    blinkyDancer = new BlinkyDancer(10, 20, timeBetweenSteps);
   });
 
   it("should have a jQuery $node object", function() {
@@ -23,10 +23,11 @@ describe("blinkyDancer", function() {
     it("should call step at least once per second", function() {
       sinon.spy(blinkyDancer, "step");
       expect(blinkyDancer.step.callCount).to.be.equal(0);
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+   // blinkyDancer = new BlinkyDancer(10, 20, timeBetweenSteps);
       clock.tick(timeBetweenSteps);
-
-      expect(blinkyDancer.step.callCount).to.be.equal(1);
+      //debugger;
+      clock.tick(timeBetweenSteps);
+     expect(blinkyDancer.step.callCount).to.be.equal(1);
 
       clock.tick(timeBetweenSteps);
       expect(blinkyDancer.step.callCount).to.be.equal(2);
